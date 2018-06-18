bcd2424228e0443b31df27973a31ebf71497f8c4
Jair Avilés <hi@jairaviles.mx>
2018-06-17 19:31:32 -0500

    update terraform main file - add loadbalancer and tags configuration

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,35 @@
+resource "digitalocean_loadbalancer" "devops-demo" {
+  name = "plazi-html-v2"
+  region = "sfo2"
+
+  forwarding_rule {
+    entry_port = 80
+    entry_protocol = "http"
+
+    target_port = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port = 3000
+    protocol = "http"
+  }
+
+  droplet_tag = "${digitalocean_tag.devops-demo.name}"
+}
+
+resource "digitalocean_tag" "devops-demo" {
+  name = "platzi-html"
+}
+
 resource "digitalocean_droplet" "devops-demo" {
   image  = "35366897"
   name   = "platzi-demo-v2"
   region = "sfo2"
   size   = "512mb"
   ssh_keys = [21651223]
+  tags = ["${digitalocean_tag.devops-demo.id}"]
+
   user_data = <<EOF
 #cloud-config
 coreos:
