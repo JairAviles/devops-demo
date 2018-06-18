6ee6a7870b223c6b05577aaffc0c85b4e2ca4aa2
Jair Avilés <hi@jairaviles.mx>
2018-06-17 23:31:37 -0500

    fix minor issues

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,5 +1,5 @@
-wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.1/packer_1.2.1_linux_amd64.zip?_ga=2.172604963.1433869664.1522083868-384145405.1522083868
-wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip?_ga=2.42370918.2146529047.1522086131-369822613.1522086131
+wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_linux_amd64.zip
+wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 sudo unzip /tmp/packer.zip -d /bin
 sudo unzip /tmp/terraform.zip -d /bin
 
